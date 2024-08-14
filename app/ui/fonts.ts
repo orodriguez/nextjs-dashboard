8fd8636487a8ccf3ccbb67ac5b2a21c0735a90e0
“orodriguez” <omarjavier15@gmail.com>
2024-08-14 13:53:20 -0400

    Optimizing Fronts and Images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'] });
